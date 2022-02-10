6ceeb7953e4273f1eacd0ae43604515156016774
brunofelixf <brunofelixf@gmail.com>
2022-02-10 19:56:32 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './components/Button'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <input></input>
+        <Button>Adicionar</Button>
+        <tarefas></tarefas>
+      </div>
+    </>
+  )
+}
+
+export default App
